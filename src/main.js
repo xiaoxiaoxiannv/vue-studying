77ac71a90fd8f74affbe42f2a0b83a5920476dd6
Luna <1017929760@qq.com>
2020-12-21 23:04:15 +0800

    比较vue.min.js和vue.runtime.min.js的区别

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+console.log(window.Vue)
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+ //import App from './App.vue'
+//
+// Vue.config.productionTip = false
+//
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render(h){
+    return h('div',[this.n,h('button',{on:{click:this.add}},'+1')])
+  },
+  data:{
+    n:0
+  },
+  methods:{
+    add(){
+      this.n += 1
+    }
+  }
+})
