dd0d31814bdf7c8e89e5fde24e70b04ba85aa1de
Luna <1017929760@qq.com>
2020-12-22 09:27:44 +0800

    Vue单文件组件

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
-console.log(window.Vue)
 import Vue from 'vue'
  //import App from './App.vue'
 //
 // Vue.config.productionTip = false
-//
+import Demo from './Demo'
 new Vue({
-  el: '#app',
+  el:'#app',
   render(h){
-    return h('div',[this.n,h('button',{on:{click:this.add}},'+1')])
-  },
-  data:{
-    n:0
-  },
-  methods:{
-    add(){
-      this.n += 1
-    }
+    return h(Demo)
   }
 })
