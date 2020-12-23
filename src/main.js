d5adb7d3f1df81e9573c5b4c44fc060bc4149a91
Luna <1017929760@qq.com>
2020-12-23 11:05:58 +0800

    data可以是对象也可以是函数,但优先使用函数

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
- //import App from './App.vue'
-//
+console.log(window.Vue)
+const Vue = window.Vue
 Vue.config.productionTip = false
-import Demo from './Demo'
+//import Demo from './Demo'
+
 new Vue({
-  render(h){
-    return h(Demo)
+    data(){
+        return{
+            n: 0
+        }
+    },
+    template: `
+        <div class="red">
+            {{n}}
+            <button @click="add">+1</button>
+        </div>
+     `,
+  methods: {
+      add(){
+        this.n += 1
+      }
   }
 }).$mount('#app')
