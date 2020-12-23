90383634987a05d7162cebafd7f2a8982134ccb5
Luna <1017929760@qq.com>
2020-12-23 13:49:48 +0800

    用computed展示列表，如果依赖的属性没有变化，就不会重新被计算

--- src/main.js
+++ src/main.js
@@ -3,29 +3,48 @@ const Vue = window.Vue
 Vue.config.productionTip = false
 //import Demo from './Demo.vue'
 
+let id = 0;
+const createUser = (name, gender) => {
+    id += 1;
+    return {id: id, name: name, gender: gender}
+}
 new Vue({
-    data: {
-        user:{
-            emial: "fangyinghang@qq.com",
-            nickname: "方方",
-            phone: "13812312312"
+    data() {
+        return {
+            users: [
+                createUser('方方', '男'),
+                createUser('圆圆', '女'),
+                createUser('小新', '男'),
+                createUser('小葵', '女')
+            ],
+            gender: ''
         }
     },
-    computed:{
-      displayName:{
-          get(){
-              const user = this.user
-              return user.nickname || user.phone || user.emial
-          },
-          set(value){
-            this.user.nickname = value
-          }
-      }
+    computed: {
+        displayUsers() {
+            console.log('计算了一次')
+            const hash = {
+                male: '男',
+                female: '女'
+            }
+            const {users, gender} = this
+            if (gender === '') {
+                return users
+            } else if (typeof gender === 'string') {
+                return users.filter(u => u.gender === hash[gender])
+            } else  {
+                throw new Error('gender的值是意外的值')
+            }
+        }
     },
     template: `
         <div>
-            {{displayName}}
-            <button @click="displayName='圆圆'">set</button>
+            <button @click="gender = ''">全部</button>
+            <button @click="gender = 'male'">男</button>
+            <button @click="gender = 'female'">女</button>
+            <ul>
+                <li v-for="(u,index) in displayUsers" :key="index">{{u.name}} - {{u.gender}}</li>
+            </ul>
         </div>
     `,
 }).$mount('#app')
