dbc9863da7c0c4bcf41c686183b14155623847ce
Luna <1017929760@qq.com>
2020-12-23 14:07:16 +0800

    用watch做撤销功能

--- src/main.js
+++ src/main.js
@@ -3,48 +3,52 @@ const Vue = window.Vue
 Vue.config.productionTip = false
 //import Demo from './Demo.vue'
 
-let id = 0;
-const createUser = (name, gender) => {
-    id += 1;
-    return {id: id, name: name, gender: gender}
-}
 new Vue({
-    data() {
-        return {
-            users: [
-                createUser('方方', '男'),
-                createUser('圆圆', '女'),
-                createUser('小新', '男'),
-                createUser('小葵', '女')
-            ],
-            gender: ''
+    data: {
+        n: 0,
+        history: [],
+        inUndoMode: false
+    },
+    watch: {
+        n(newValue, oldValue) {
+            if(!this.inUndoMode){
+                this.history.push({from: oldValue, to: newValue})
+            }
         }
     },
-    computed: {
-        displayUsers() {
-            console.log('计算了一次')
-            const hash = {
-                male: '男',
-                female: '女'
-            }
-            const {users, gender} = this
-            if (gender === '') {
-                return users
-            } else if (typeof gender === 'string') {
-                return users.filter(u => u.gender === hash[gender])
-            } else  {
-                throw new Error('gender的值是意外的值')
-            }
+    methods: {
+        add1() {
+            this.n += 1
+        },
+        add2() {
+            this.n += 2
+        },
+        minus1() {
+            this.n -= 1
+        },
+        minus2() {
+            this.n -= 2
+        },
+        undo(){
+            const last = this.history.pop()
+            const old = last.from
+            this.inUndoMode = true
+            this.n = old;
+            this.$nextTick(()=>{
+                this.inUndoMode = false
+            },0)
         }
     },
     template: `
         <div>
-            <button @click="gender = ''">全部</button>
-            <button @click="gender = 'male'">男</button>
-            <button @click="gender = 'female'">女</button>
-            <ul>
-                <li v-for="(u,index) in displayUsers" :key="index">{{u.name}} - {{u.gender}}</li>
-            </ul>
+            {{n}}
+            <button @click="add1">+1</button>
+            <button @click="add2">+2</button>
+            <button @click="minus1">-1</button>
+            <button @click="minus2">-2</button>
+            <hr>
+            {{history}}
+            <button @click="undo">撤销</button>
         </div>
     `,
 }).$mount('#app')
