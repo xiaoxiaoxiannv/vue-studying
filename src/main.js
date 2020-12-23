d131209adc046b40db2bf81871c94ef561b6fdff
Luna <1017929760@qq.com>
2020-12-23 15:21:52 +0800

    使用@click.prevent

--- src/main.js
+++ src/main.js
@@ -1,54 +1,20 @@
-console.log(window.Vue)
 const Vue = window.Vue
 Vue.config.productionTip = false
 //import Demo from './Demo.vue'
 
 new Vue({
-    data: {
-        n: 0,
-        history: [],
-        inUndoMode: false
-    },
-    watch: {
-        n(newValue, oldValue) {
-            if(!this.inUndoMode){
-                this.history.push({from: oldValue, to: newValue})
-            }
-        }
-    },
+    data: {n: 0,},
     methods: {
-        add1() {
+        add() {
             this.n += 1
         },
-        add2() {
-            this.n += 2
-        },
-        minus1() {
-            this.n -= 1
-        },
-        minus2() {
-            this.n -= 2
-        },
-        undo(){
-            const last = this.history.pop()
-            const old = last.from
-            this.inUndoMode = true
-            this.n = old;
-            this.$nextTick(()=>{
-                this.inUndoMode = false
-            },0)
-        }
     },
     template: `
         <div>
             {{n}}
-            <button @click="add1">+1</button>
-            <button @click="add2">+2</button>
-            <button @click="minus1">-1</button>
-            <button @click="minus2">-2</button>
+            <button @click="add">+1</button>
             <hr>
-            {{history}}
-            <button @click="undo">撤销</button>
+            <a @click.prevent href="https://www.baidu.com">baidu</a>
         </div>
     `,
 }).$mount('#app')
