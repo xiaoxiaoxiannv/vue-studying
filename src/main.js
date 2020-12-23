56feb0d3285d3c032daa3ad02ebad515594b1bd2
Luna <1017929760@qq.com>
2020-12-23 13:24:14 +0800

    使用computed

--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
 console.log(window.Vue)
 const Vue = window.Vue
 Vue.config.productionTip = false
-import Demo from './Demo.vue'
+//import Demo from './Demo.vue'
 
 new Vue({
-    components: {Demo},
     data: {
-        visible: true,
-        n: 0
+        user:{
+            emial: "fangyinghang@qq.com",
+            nickname: "方方",
+            phone: "13812312312"
+        }
+    },
+    computed:{
+      displayName:{
+          get(){
+              const user = this.user
+              return user.nickname || user.phone || user.emial
+          },
+          set(value){
+            this.user.nickname = value
+          }
+      }
     },
     template: `
         <div>
-            {{n}}
-            <Demo :message="n" :fn="add"></Demo>
+            {{displayName}}
+            <button @click="displayName='圆圆'">set</button>
         </div>
     `,
-    methods: {
-        add(){
-            this.n += 1
-        },
-        toggle() {
-            this.visible = !this.visible
-        }
-    }
 }).$mount('#app')
