2197f9b22600755edccbf857c43a22656b2e8f4b
Luna <1017929760@qq.com>
2020-12-23 10:22:15 +0800

    el可用$mount代替

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
  //import App from './App.vue'
 //
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 import Demo from './Demo'
 new Vue({
-  el:'#app',
   render(h){
     return h(Demo)
   }
-})
+}).$mount('#app')
