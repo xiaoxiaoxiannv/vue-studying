41eb32594e64881331ba477044f89bb51efc0296
Luna <1017929760@qq.com>
2020-12-24 11:13:26 +0800

    修饰符.trim去掉两头空格

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <form @submit.prevent="onSubmit">
             <label>
                 <span>用户名</span>
-                <input type="text" v-model.number="user.username"/>
+                <input type="text" v-model.trim="user.username"/>
             </label>
             <label>
                 <span>密码</span>
