9e675c9e47f3b137cb82098a26f5d7fe68de5867
Luna <1017929760@qq.com>
2020-12-24 10:28:19 +0800

    使用v-model input

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,23 @@
 <template>
-    <div :class="`app theme-${themeName} fontSize-${fontSizeName}`">
-        <Child1/>
-        <button>x</button>
-        <Child2/>
-        <button>x</button>
-        <Child3/>
-        <button>x</button>
-        <Child4/>
-        <button>x</button>
-        <Child5/>
-        <button>x</button>
+    <div id="app">
+        <input v-model="message" placeholder="edit me"/>
+        <p>Message is:{{message}}</p>
+        <hr/>
+        <button @click="message = 'frank'">set message to frank</button>
     </div>
 </template>
 
 <script>
-    import Child1 from "./components/Child1.vue";
-    import Child2 from "./components/Child2.vue";
-    import Child3 from "./components/Child3.vue";
-    import Child4 from "./components/Child4.vue";
-    import Child5 from "./components/Child5.vue";
     export default {
-        provide(){
-          return{
-              themeName: this.themeName,
-              changeTheme: this.changeTheme,
-              changeFontSize: this.changeFontSize
-          }
-        },
+        name:'App',
         data(){
             return{
-                themeName: "blue",
-                fontSizeName:'normal'
+                message:undefined
             }
         },
-        methods:{
-          changeTheme(){
-              if(this.themeName === 'blue'){
-                  this.themeName = 'red'
-              }else {
-                  this.themeName = 'blue'
-              }
-          },
-            changeFontSize(name){
-              if(['normal','big','small'].indexOf(name)>=0){
-                  this.fontSizeName = name
-              }
-            }
-        },
-        components:{Child1,Child2,Child3,Child4,Child5}
+        components:{}
     }
 </script>
 
 <style lang="scss">
-    .app.theme-blue button{
-        background: blue;
-        color: white;
-    }
-    .app.theme-blue {
-        color: darkblue;
-    }
-    .app.theme-red button{
-        background: red;
-        color: white;
-    }
-    .app.theme-red {
-        color: darkred;
-    }
-    .app.fontSize-normal{
-        font-size: 16px;
-    }
-    .app.fontSize-big{
-        font-size: 20px;
-    }
-    .app.fontSize-small{
-        font-size: 12px;
-    }
-    .app button{
-        font-size: inherit;
-    }
 </style>
