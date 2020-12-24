dbf3dfa9c22dc73037fab9856158c6261592d5bd
Luna <1017929760@qq.com>
2020-12-24 10:36:44 +0800

    多个复选框绑定到数组上

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
     <div id="app">
+        爱好:{{x}}
         <label>
-            <input type="checkbox" v-model="x"/>
-            <span>x:{{x}}</span>
+            <input type="checkbox" v-model="x" :value="1"/>
+            <span>看电影</span>
+        </label>
+        <label>
+            <input type="checkbox" v-model="x" :value="2"/>
+            <span>游泳</span>
+        </label>
+        <label>
+            <input type="checkbox" v-model="x" :value="3"/>
+            <span>徒步</span>
         </label>
     </div>
 </template>
@@ -12,7 +21,7 @@
         name:'App',
         data(){
             return{
-                x:true
+                x:[]
             }
         },
         components:{}
